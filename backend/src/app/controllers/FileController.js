5ef134d66a13366933f254dae27f91a9ad4e9c04
Emilio Schaedler Heinzmann <emilio.heinzmann@cwi.com.br>
2019-06-29 10:59:14 -0300

    fix: fixing file controller

--- backend/src/app/controllers/FileController.js
+++ backend/src/app/controllers/FileController.js
@@ -1,6 +1,15 @@
+import File from '../models/File'
+
 class FileController {
-  store(req, res) {
-    return res.json(req.file)
+  async store(req, res) {
+    const { originalname: name, filename: path } = req.file
+
+    const file = await File.create({
+      name,
+      path,
+    })
+
+    return res.json(file)
   }
 }
 

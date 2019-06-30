b357de29272593dbabd0fc28d17a93278c6e11ad
Emilio Schaedler Heinzmann <emilio.heinzmann@cwi.com.br>
2019-06-30 13:40:05 -0300

    feat: email notification when user cancel a appointment

--- backend/src/app/controllers/AppoitmentController.js
+++ backend/src/app/controllers/AppoitmentController.js
@@ -8,6 +8,8 @@ import File from '../models/File'
 
 import Notification from '../schemas/Notification'
 
+import Mail from '../../lib/Mail'
+
 class AppointmentController {
   async index(req, res) {
     const { page = 1 } = req.query
@@ -119,7 +121,20 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id)
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
+    })
 
     if (appointment.user_id !== req.userId) {
       return res.status(401).json({
@@ -138,6 +153,21 @@ class AppointmentController {
     appointment.canceled_at = new Date()
 
     await appointment.save()
+
+    const { provider, user, date } = appointment
+
+    await Mail.sendMail({
+      to: `${provider.name} <${provider.email}>`,
+      subject: 'Agendamento cancelado',
+      template: 'cancellation',
+      context: {
+        provider: provider.name,
+        user: user.name,
+        date: format(date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
+    })
 
     return res.json(appointment)
   }

d653355ca090e2ecad858576bc9248823101c9e5
Emilio Heinzmann <emiliosheinz@gmail.com>
2019-07-20 10:16:51 -0300

    Creating signin elements in screen

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import logo from '~/assets/logo.svg'
 
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <h1>SignIn</h1>
+  return (
+    <>
+      <img src={logo} alt='GoBarber logo' />
+
+      <form>
+        <input type='email' placeholder='Seu e-mail' />
+        <input type='passwrod' placeholder='Sua senha secreta' />
+
+        <button type='submit'>Acessar</button>
+        <Link to='/register'>Criar conta gratuita</Link>
+      </form>
+    </>
+  )
 }

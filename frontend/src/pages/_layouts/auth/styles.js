fb3af30b2f587a471092b85cef6a90c1b9f66b4f
Emilio Heinzmann <emiliosheinz@gmail.com>
2019-07-20 12:05:32 -0300

    Fixing validation style

--- frontend/src/pages/_layouts/auth/styles.js
+++ frontend/src/pages/_layouts/auth/styles.js
@@ -35,10 +35,9 @@ export const Content = styled.div`
   }
 
   span {
-    color: #fb6f91;
+    color: #fff;
     align-self: flex-start;
     margin: 0 0 10px;
-    font-weight: bold;
   }
 
   button {

fdce02d713a8a816a91a805a5bdf033e9708fee1
Emilio Heinzmann <emiliosheinz@gmail.com>
2019-07-22 19:01:33 -0300

    Creating register to the application

--- frontend/src/store/modules/auth/sagas.js
+++ frontend/src/store/modules/auth/sagas.js
@@ -31,4 +31,26 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)])
+export function* signUp({ payload }) {
+  const { name, email, password } = payload
+
+  try {
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    })
+
+    history.push('/')
+  } catch (err) {
+    toast.error('Falha no cadastro, verifique os seus dados.')
+
+    yield put(signFailure())
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+])

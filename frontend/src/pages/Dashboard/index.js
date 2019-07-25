2b94e872f2256541ed88014436d647e45437cb53
Emilio Heinzmann <emiliosheinz@gmail.com>
2019-07-25 12:33:15 -0300

    Creating style to the dashboard page

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,10 +1,39 @@
 import React from 'react'
-import api from '~/services/api'
 
-// import { Container } from './styles';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
+
+import { Container, Time } from './styles'
 
 export default function Dashboard() {
-  api.get('appointments')
-
-  return <h1>Dashboard</h1>
+  return (
+    <Container>
+      <header>
+        <button type='button'>
+          <MdChevronLeft color='#fff' size={36} />
+        </button>
+        <strong>31 de maio</strong>
+        <button type='button'>
+          <MdChevronRight color='#fff' size={36} />
+        </button>
+      </header>
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Emlio Heinzman</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Emlio Heinzman</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Emlio Heinzman</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Emlio Heinzman</span>
+        </Time>
+      </ul>
+    </Container>
+  )
 }

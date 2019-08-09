fb4826e328a5cc6ff61755bc9bcfdafe4830ce99
Emilio Heinzmann <emiliosheinz@gmail.com>
2019-08-09 20:50:33 -0300

    Creating profile screen and fixing api connection

--- mobile/src/pages/Profile/index.js
+++ mobile/src/pages/Profile/index.js
@@ -1,21 +1,58 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import Icon from 'react-native-vector-icons/MaterialIcons'
+
+import { updateProfileRequest } from '~/store/modules/user/actions'
+import { signOut } from '~/store/modules/auth/actions'
 
 import Background from '~/components/Background'
 
-import { Container, Title, Form, FormInput, Separator } from './styles'
+import {
+  Container,
+  Title,
+  Form,
+  FormInput,
+  Separator,
+  SubmitButton,
+  LogoutButton,
+} from './styles'
 
 export default function Profile() {
+  const dispatch = useDispatch()
+  const profile = useSelector(state => state.user.profile)
+
   const emailRef = useRef()
   const passwordRef = useRef()
   const oldPasswordRef = useRef()
   const passwordConfirmationRef = useRef()
 
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
+  const [name, setName] = useState(profile.name)
+  const [email, setEmail] = useState(profile.email)
   const [oldPassword, setOldPassword] = useState('')
   const [password, setPassword] = useState('')
   const [passwordConfirmation, setPasswordConfirmation] = useState('')
+
+  useEffect(() => {
+    setOldPassword('')
+    setPassword('')
+    setPasswordConfirmation('')
+  }, [profile])
+
+  function handleSubmit() {
+    dispatch(
+      updateProfileRequest({
+        name,
+        email,
+        oldPassword,
+        password,
+        passwordConfirmation,
+      })
+    )
+  }
+
+  function handleLogout() {
+    dispatch(signOut())
+  }
 
   return (
     <Background>
@@ -79,6 +116,7 @@ export default function Profile() {
           />
 
           <SubmitButton onPress={handleSubmit}>Atualizar Perfil</SubmitButton>
+          <LogoutButton onPress={handleLogout}>Sair</LogoutButton>
         </Form>
       </Container>
     </Background>
